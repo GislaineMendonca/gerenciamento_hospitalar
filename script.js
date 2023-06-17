371e5e3f7bad61a4acbe3c702cef381bf2c10753
Gislaine S. Mendonça <gislainesmendonca@gmail.com>
2023-06-17 14:13:38 -0300

    Alterações na tela de login e na tela inicial

---
+++ script.js
@@ -0,0 +1,20 @@
+const carousel = document.querySelector('.carousel');
+const carouselInner = document.querySelector('.carousel-inner');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+
+let currentIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    carouselInner.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex < 2) {
+    currentIndex++;
+    carouselInner.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+  }
+});
